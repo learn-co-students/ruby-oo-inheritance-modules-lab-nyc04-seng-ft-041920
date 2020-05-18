49873b3d33ec9a630f42ed7da2c263dcb9f48ef1
senadakadric <skadric@fordham.edu>
2020-05-18 19:15:53 -0400

    adding memorable module

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,10 @@
 require 'pry'
 
 class Artist
+
+  #using the extend keyword to make the class methods in the module Memorable usable in this class
+  extend Memorable
+
   attr_accessor :name
   attr_reader :songs
 
@@ -11,22 +15,27 @@ class Artist
     @songs = []
   end
 
+  #class method, gives you the instance of an artist that matches the name that is passed in
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
 
+  #returns an array of all artists in the class
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  #empties the entire artist class array
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # #tells you how many artist instances are in the artist class array
+  # def self.count
+  #   self.all.count
+  # end
 
+  
   def add_song(song)
     @songs << song
     song.artist = self
@@ -36,6 +45,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
+  #gsub method (in this particular case) will take all space occurrences and replace them with dashes
   def to_param
     name.downcase.gsub(' ', '-')
   end
